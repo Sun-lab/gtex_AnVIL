ecb21df3969af10820f96db4bf40a7a6ff404035
Wei Sun <weisun2013@gmail.com>
2020-01-09 16:31:48 -0800

    initial commit

--- get_TReC.R
+++ get_TReC.R
@@ -9,11 +9,12 @@ filenames = list.files(path="~/research/data/_GTEx/v8/RNAseq",
                        pattern=".bam$", full.names=TRUE)
 filenames
 
-bamfiles  = BamFileList(filenames, yieldSize=1000000)
-bamfiles
+fname   = filenames[1]
+bamfile = BamFileList(fname, yieldSize=1000000)
+bamfile
 
 date()
-se = summarizeOverlaps(features=genes, reads=bamfiles, mode="Union",
+se = summarizeOverlaps(features=genes, reads=bamfile, mode="Union",
              singleEnd=FALSE, ignore.strand=TRUE, fragments=TRUE)
 date()
 
@@ -23,19 +24,13 @@ rowRanges(se)
 str(metadata(rowRanges(se)))
 
 as1 = assay(se)
-
+as1 = as.data.frame(as1)
+dim(as1)
 head(as1)
-cts = rbind(cts, colSums(as1))
 
-setwd(pipeline_dir)
-write.table(as1, file = sprintf("%s/%s_gene_level_counts_filterIt_total.txt", proj_output, sam), append = FALSE,
+write.table(as1, file = sprintf("%s_TReC.txt", fname), append = FALSE,
   quote = FALSE, sep = "\t", eol = "\n", row.names = TRUE,
   col.names = TRUE)
-
-row.names(cts) = c("nMappedReads", "nFragMappedToGene")
-write.table(cts, file = sprintf("%s/%s_TReC_liberal_filterIt_total.txt", proj_output, sam), append = FALSE,
-quote = FALSE, sep = "\t", eol = "\n", row.names = TRUE,
-col.names = TRUE)
 
 sessionInfo()
 q(save="no")
